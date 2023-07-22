480d5de066b2f7fd5aba330a12ec49f2b435130b
aws69 <awsalomari11469@gmail.com>
2023-07-22 20:06:10 +0300

    log in and sign up

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,51 +1,59 @@
 import { useState } from 'react';
 import SignUp from "../SignUp/SignUp";
 
-function login () {
-
-    
-    
-    const [inputs, setinputs] = useState({
+function Login() {
+    const [inputs, setInputs] = useState({
         email: "",
         password: ""
     });
 
-    const [warnemail, setwarnemail] = useState(false);
-    const [warnpass, setwarnpass] = useState(false);
-    const [danger, setdanger] = useState(true);
-
-    const [eye, seteye] = useState(true);
-    const [pass, setpass] = useState("password");
-
+    const [warnEmail, setWarnEmail] = useState(false);
+    const [warnPass, setWarnPass] = useState(false);
+    const [danger, setDanger] = useState(true);
+    const [eye, setEye] = useState(true);
+    const [passType, setPassType] = useState("password");
 
     const inputEvent = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        if (name == "email") {
+        if (name === "email") {
             if (value.length > 0) {
-                setdanger(true);
+                setDanger(true);
             }
         }
-        setinputs((lastValue) => {
+        setInputs((lastValue) => {
             return {
                 ...lastValue,
                 [name]: value
-            }
+            };
         });
     };
 
     const submitForm = (e) => {
         e.preventDefault();
-        setwarnemail(false);
-        setwarnpass(false);
-        if (inputs.email.length < 1) { setdanger(false); } if (inputs.email == "") { setwarnemail(true); } else if (inputs.password == "") { setwarnpass(true); } else { alert("Logged in Successfully"); }
-    }; const Eye = () => {
-        if (pass == "password") {
-            setpass("text");
-            seteye(false);
+        setWarnEmail(false);
+        setWarnPass(false);
+
+        if (inputs.email.length < 1) {
+            setDanger(false);
+        }
+
+        if (inputs.email === "") {
+            setWarnEmail(true);
+        } else if (inputs.password === "") {
+            setWarnPass(true);
         } else {
-            setpass("password");
-            seteye(true);
+            alert("Logged in Successfully");
+        }
+    };
+
+    const togglePasswordVisibility = () => {
+        if (passType === "password") {
+            setPassType("text");
+            setEye(false);
+        } else {
+            setPassType("password");
+            setEye(true);
         }
     };
 
@@ -54,47 +62,58 @@ function login () {
             <div className="container">
                 <div className="card">
                     <div className="form">
-                        <div className="left-side">
-                            <img src="https://imgur.com/XaTWxJX.jpg" />
+                        <div className="right-side">
+                            <img src="https://imgur.com/XaTWxJX.jpg" alt="Logo" />
                         </div>
 
-                        <div className="right-side">
+                        <div className="left-side">
                             <div className="register">
-                                <p>Not a member? <link to={<SignUp />}>Register Now</link></p>
+                                <p>
+                                    Not a member? <SignUp />
+                                </p>
                             </div>
 
                             <div className="hello">
                                 <h2>Hello Again!</h2>
-                                <h4>Welcome back you have been missed! </h4>
+                                <h4>Welcome back; you have been missed!</h4>
                             </div>
 
                             <form onSubmit={submitForm}>
-
                                 <div className="input_text">
-                                    <input className={` ${warnemail ? "warning" : ""}`} type="text" placeholder="Enter Email" name="email" value={inputs.email} onChange={inputEvent} />
-                                    <p className={` ${danger ? "danger" : ""}`}><i className="fa fa-warning"></i>Please enter a valid email address.</p>
+                                    <input
+                                        className={`${warnEmail ? "warning" : ""}`}
+                                        type="text"
+                                        placeholder="Enter Email"
+                                        name="email"
+                                        value={inputs.email}
+                                        onChange={inputEvent}
+                                    />
+                                    <p className={`${danger ? "danger" : ""}`}>
+                                        <i className="fa fa-warning"></i>Please enter a valid email address.
+                                    </p>
                                 </div>
                                 <div className="input_text">
-                                    <input className={` ${warnpass ? "warning" : ""}`} type={pass} placeholder="Enter Password" name="password" value={inputs.password} onChange={inputEvent} />
-                                    <i onClick={Eye} className={`fa ${eye ? "fa-eye-slash" : "fa-eye"}`}></i>
+                                    <input
+                                        className={`${warnPass ? "warning" : ""}`}
+                                        type={passType}
+                                        placeholder="Enter Password"
+                                        name="password"
+                                        value={inputs.password}
+                                        onChange={inputEvent}
+                                    />
+                                    <i onClick={togglePasswordVisibility} className={`fa ${eye ? "fa-eye-slash" : "fa-eye"}`}></i>
                                 </div>
                                 <div className="btn">
-                                    <button type="submit">Sign in</button>
+                                    <button type="submit">Log In</button>
                                 </div>
-
                             </form>
-
                             <hr />
                         </div>
                     </div>
                 </div>
             </div>
-
-
-
         </>
     );
 }
 
-
-export default login;
+export default Login;
