4c37dd5820bf53e56011f13c8b5a900422bd85a6
farahisJoy <farahelaydi9@gmail.com>
2023-07-23 18:18:25 +0000

    m

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Footer from "../Footer/Footer";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import search from "../Assest/search.jpg";
 import Interview from "../Assest/interview.jpg";
@@ -119,7 +118,6 @@ function Home() {
         </span>
       </div>
 
-      <Footer />
     </div>
   );
 }
--- src/components/SavedJob/SavedJob.js
+++ src/components/SavedJob/SavedJob.js
@@ -1,9 +1,7 @@
-import Footer from "../Footer/Footer";
 import SavedCard from "../SavedCard/SavedCard";
 const SavedJob = () => {
   return <div>
     <SavedCard/>
-    <Footer/>
   </div>;
 };
 export default SavedJob;
