8434e0b5e3636ff7bb8083d93fca646eca6638c8
farahisJoy <farahelaydi9@gmail.com>
2023-07-23 17:58:51 +0000

    kl

--- src/components/NavBarNotLog/NavBarNotLog.js
+++ src/components/NavBarNotLog/NavBarNotLog.js
@@ -3,71 +3,57 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-// import './NavBarNotLog.css';
-import logo from '../Assest/logo.png'
+import logo from '../Assest/logo.png';
 import { useAuth0 } from '@auth0/auth0-react';
 
-
 function NavBarNotLog() {
-  const { logout, isAuthenticated , loginWithRedirect} = useAuth0();
+  const { logout, isAuthenticated, loginWithRedirect } = useAuth0();
 
   return (
     <div>
       <Navbar className="bg-body-tertiary" style={{ height: '80px' }}>
         <Container>
           <Navbar.Brand href="#home" className="d-flex align-items-center navbar-brand">
-            <img
-              alt=""
-              src={logo}
-              width="110"
-              height="120"
-            />
-            <span>JobScouts</span>
+            <img alt="" src={logo} width="110" height="120" />
+            <span style={{ fontSize: '2.1rem', fontWeight: 'bolder', color: '#006c67', marginLeft: '-35px', fontFamily: "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif" }}>JobScouts</span>
           </Navbar.Brand>
-          <Nav variant="underline" defaultActiveKey="/home">
-
+          <Nav variant="underline" defaultActiveKey="/home" className="d-flex align-items-center">
             <Nav.Item>
-              <Nav.Link href="/" className="nav-link">Home</Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link href='/About' className="nav-link">AboutUs</Nav.Link>
-            </Nav.Item>
-
-            {/* LogIn Button */}
-            {!isAuthenticated && (
-              <Nav.Item>
-              <Nav.Link eventKey="link-1">
-
-                <Button variant="outline-success" className="btn-outline-success" onClick={() => { loginWithRedirect() }}>LogIn</Button>
+              <Nav.Link href="/" className="nav-link">
+                Home
               </Nav.Link>
             </Nav.Item>
-            )}
-            
 
-            {/* LogOut Button */}
+            <Nav.Item>
+              <Nav.Link href="/About" className="nav-link">
+                AboutUs
+              </Nav.Link>
+            </Nav.Item>
+
             {isAuthenticated && (
-             <div>
               <Nav.Item>
-              <Nav.Link href='/SavedJob' className="nav-link">saved job</Nav.Link>
-            </Nav.Item>
-
-              <Nav.Item>
-              <Nav.Link eventKey="link-1">
-                <Button variant="outline-success" className="btn-outline-success" onClick={()=>{logout()}}>LogOut</Button>
-              </Nav.Link>
-            </Nav.Item>
-            </div>
+                <Nav.Link href="/SavedJob" className="nav-link">
+                My Jobs
+                </Nav.Link>
+              </Nav.Item>
             )}
-
-
-            {/* <Nav.Item>
-              <Nav.Link eventKey="link-1">
-                <Button variant="outline-success" className="btn-outline-success">SignUp</Button>
-              </Nav.Link>
-            </Nav.Item> */}
-
-          
+            {!isAuthenticated ? (
+              <Nav.Item>
+                <Nav.Link eventKey="link-1">
+                  <Button variant="success" className="success" onClick={() => { loginWithRedirect() }}>
+                    LogIn
+                  </Button>
+                </Nav.Link>
+              </Nav.Item>
+            ) : (
+              <Nav.Item>
+                <Nav.Link eventKey="link-1">
+                  <Button variant="success" className="success" onClick={() => { logout() }}>
+                    LogOut
+                  </Button>
+                </Nav.Link>
+              </Nav.Item>
+            )}
           </Nav>
         </Container>
       </Navbar>
--- src/components/SavedCard/SavedCard.js
+++ src/components/SavedCard/SavedCard.js
@@ -3,11 +3,17 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import company from '../Assest/company.jpg';
 import './SavedCard.css';
+import Details from "../Details/Details";
+import { useState } from "react";
 
 const SavedCard = () => {
+  const [showModal, setShowModal] = useState(false);
+    const handleShowModal = () => {
+      setShowModal(true);
+    };
   return (
     <div className="center">
-      <Card border="success" className="card">
+      <Card className="card">
         <Card.Body>
           <div className="card-content">
             <img src={company} alt="Card Image" className="card-image" />
@@ -20,47 +26,13 @@ const SavedCard = () => {
             </div>
           </div>
           <div className="card-buttons">
-            <Button variant="secondary">More Details</Button>
+            <Button variant="secondary" onClick={handleShowModal}>More Details</Button>
             <Button variant="primary">Apply To Job</Button>
           </div>
         </Card.Body>
       </Card>
-      <Card border="success" className="card">
-        <Card.Body>
-          <div className="card-content">
-            <img src={company} alt="Card Image" className="card-image" />
-            <div className="card-text">
-              <Card.Title>Job Title</Card.Title>
-              <Card.Text>
-                <p>City, Country</p>
-                <p>Min Salary - Max Salary</p>
-              </Card.Text>
-            </div>
-          </div>
-          <div className="card-buttons">
-            <Button variant="secondary">More Details</Button>
-            <Button variant="primary">Apply To Job</Button>
-          </div>
-        </Card.Body>
-      </Card>
-      <Card border="success" className="card">
-        <Card.Body>
-          <div className="card-content">
-            <img src={company} alt="Card Image" className="card-image" />
-            <div className="card-text">
-              <Card.Title>Job Title</Card.Title>
-              <Card.Text>
-                <p>City, Country</p>
-                <p>Min Salary - Max Salary</p>
-              </Card.Text>
-            </div>
-          </div>
-          <div className="card-buttons">
-            <Button variant="secondary">More Details</Button>
-            <Button variant="primary">Apply To Job</Button>
-          </div>
-        </Card.Body>
-      </Card>
+      {showModal && (<Details/>)}
+
     </div>
   );
 };
