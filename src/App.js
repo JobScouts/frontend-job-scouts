f241991866e8a4d7c895001a97660240719191b1
MohamadSamara <mohamadsamara1211@gmail.com>
2023-07-24 16:02:21 +0000

    mohamad

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SavedJob from './components/SavedJob/SavedJob';
 import Footer from './components/Footer/Footer'
 import ProfTese from './components/profTese';
 import NavBarNotLog from './components/NavBarNotLog/NavBarNotLog';
-
+import UserProfile from './components/Profile/Profile';
 function App() {
   return (
     <div className="App">
@@ -16,6 +16,7 @@ function App() {
         <Route path='/About' element={<About />} />
         <Route path='/ProfTese' element={<ProfTese />} />
         <Route path='/SavedJob' element={<SavedJob/>}/>
+        <Route path='/UserProfile' element={<UserProfile/>}/>
       </Routes>
       <Footer/>
     </div>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Profile.css";
+import "./profile.css";
 import { useState, useEffect } from 'react';
 
 
