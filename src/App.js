1bd83abd9f40fb4c734945c67a7bf7140afe798b
MohamadSamara <mohamadsamara1211@gmail.com>
2023-07-25 07:59:46 +0000

    main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About'
-import SavedJob from './components/SavedJob/SavedJob';
 import Footer from './components/Footer/Footer'
 import NavBarNotLog from './components/NavBarNotLog/NavBarNotLog';
 import UserProfile from './components/Profile/Profile';
-
+import SavedCard from './components/SavedCard/SavedCard';
 function App() {
   return (
     <div className="App">
@@ -13,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/About' element={<About />} />
-        <Route path='/SavedJob' element={<SavedJob/>}/>
         <Route path='/UserProfile' element={<UserProfile/>}/>
+        <Route path='/SavedJob' element={<SavedCard/>}/>
       </Routes>
       <Footer />
     </div>
