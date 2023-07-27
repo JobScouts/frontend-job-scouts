79f378bc4ae8df1ec4806d57ae1d16f5dfabe66b
MohamadSamara <mohamadsamara1211@gmail.com>
2023-07-27 10:06:38 +0000

    cards

--- src/components/CustomCard/CustomCard.js
+++ src/components/CustomCard/CustomCard.js
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
-// import Card from "react-bootstrap/Card";
-// import Button from "react-bootstrap/Button";
+import React, { useState, useEffect } from "react";
 import Alert from "react-bootstrap/Alert";
 import "./CustomCard.css";
 import Details from "../Details/Details";
 import { useAuth0 } from "@auth0/auth0-react";
 import Company from "../Assest/company.jpg";
-// import AlertTitle from '@mui/material/AlertTitle';
-
 
 const CustomCard = (props) => {
   let data = props.data;
   const { user, isAuthenticated } = useAuth0();
+  const [savedJob, setSavedJob] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [selectedJob, setSelectedJob] = useState(null);
   const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      async function fetchSavedJobs() {
+        const url = `${process.env.REACT_APP_SERVER_URL}/jobs`;
+        try {
+          let response = await fetch(url);
+          let receivedData = await response.json();
+          setSavedJob(receivedData);
+        } catch (error) {
+          console.log("Error fetching saved jobs:", error.message);
+        }
+      }
+      fetchSavedJobs();
+    }
+  }, [isAuthenticated]);
 
   const handleShowModal = (job) => {
     setSelectedJob(job);
@@ -26,282 +39,92 @@ const CustomCard = (props) => {
     setShowModal(false);
   };
 
-  async function handleSaveJob(obj) {
+  const handleSaveJob = async (obj) => {
     if (isAuthenticated) {
-      let url = `${process.env.REACT_APP_SERVER_URL}/jobs`;
-      const jobData = {
-        job_title: obj.job_title,
-        employer_name: obj.employer_name,
-        employer_logo: obj.employer_logo,
-        employer_website: obj.employer_website,
-        job_highlights: obj.job_highlights,
-        job_apply_link: obj.job_apply_link,
-        sub: user.sub,
-      };
-      try {
-        let response = await fetch(url, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(jobData),
-        });
+      const jobExists = savedJob.some(
+        (savedJob) => savedJob.sub === user.sub && savedJob.job_title === obj.job_title
+      );
 
-        if (response.status === 201) {
-          alert("Added successfully");
-        } else {
-          console.log("Error:", response.statusText);
+      if (jobExists) {
+        alert("This job is already saved to your profile.");
+      } else {
+        let url = `${process.env.REACT_APP_SERVER_URL}/jobs`;
+        const jobData = {
+          job_title: obj.job_title,
+          employer_name: obj.employer_name,
+          employer_logo: obj.employer_logo,
+          employer_website: obj.employer_website,
+          job_highlights: obj.job_highlights,
+          job_apply_link: obj.job_apply_link,
+          sub: user.sub,
+        };
+        try {
+          let response = await fetch(url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(jobData),
+          });
+
+          if (response.status === 201) {
+            alert("Added successfully");
+          } else {
+            console.log("Error:", response.statusText);
+            alert("Failed to add Job");
+          }
+        } catch (error) {
+          console.log("Error:", error.message);
           alert("Failed to add Job");
         }
-      } catch (error) {
-        console.log("Error:", error.message);
-        alert("Failed to add Job");
       }
     } else {
       alert("You have to login to add jobs to your profile");
     }
-  }
+  };
+
+  const userSavedJobs = isAuthenticated && user && user.sub ? savedJob.filter((job) => job.sub === user.sub) : [];
 
   return (
     <div>
-    {
-     data.length === 0 ? (
-      <div className="alert-container">
-        <Alert
-          show={showAlert}
-          variant="success"
-          onClose={() => setShowAlert(false)}
-          dismissible
-          className="alert"
-        >
-          <Alert.Heading>Attention!</Alert.Heading>
-          <p>Job Title Or Location Is Not Found.</p>
-        </Alert>
-      </div>
-    ) : (
-     
-      <div class="con">
-      {data.map((obj, i) => (
-
-     
-  <div class="card">
-        <h3 class="title">{obj.employer_name}</h3>
-        <h3 class="title" style={{ top: "270px" }}>{obj.job_city}</h3>
-        <h3 class="title" style={{ top: "310px" }}>{obj.job_country}</h3>
-        <h3 class="title" style={{ top: "350px" }}>{obj.job_title}</h3>
-        <div class="bar">
-              <div class="emptybar"></div>
-              <div class="filledbar"></div>
+      {data.length === 0 ? (
+        <div className="alert-container">
+          <Alert
+            show={showAlert}
+            variant="success"
+            onClose={() => setShowAlert(false)}
+            dismissible
+            className="alert"
+          >
+            <Alert.Heading>Attention!</Alert.Heading>
+            <p>Job Title Or Location Is Not Found.</p>
+          </Alert>
         </div>
-        <div class="circle">
-              <circle class="stroke" cx="60" cy="60" r="50" >
-                <img className="compImg" src={obj.employer_logo === "" ? Company : obj.employer_logo} alt="employer_logo" />
-              </circle>
+      ) : (
+        <div className="con">
+          {data.map((obj, i) => (
+            <div className="card" key={i}>
+              <h2 className="Maintitle" style={{width:"100%"}}>{obj.employer_name}</h2>
+              <h5 className="title" style={{ top: "270px" }}>{obj.job_city}</h5>
+              <h5 className="title" style={{ top: "310px" }}>{obj.job_country}</h5>
+              <h5 className="title" style={{ top: "350px" }}>{obj.job_title}</h5>
+              <div className="bar">
+                <div className="emptybar"></div>
+                <div className="filledbar"></div>
+              </div>
+              <div className="circle">
+                <circle className="stroke" cx="60" cy="60" r="50">
+                  <img className="compImg" src={obj.employer_logo === "" ? Company : obj.employer_logo} alt="employer_logo" />
+                </circle>
+              </div>
+              <button className="button" onClick={() => handleSaveJob(obj)}><span>Save</span></button>
+              <button className="button" style={{ left: "200px" }} onClick={() => handleShowModal(obj)}><span>More</span></button>
+            </div>
+          ))}
         </div>
-        <button class="button"  onClick={() => handleSaveJob(obj)} > <span>Save</span>  </button>
-        <button class="button" style={{ left: "200px" }} onClick={() => handleShowModal(obj)}>  <span>More</span>  </button>
-  </div>
-
-                            ) 
-       )// End map 
-       }
-
-</div> // End con
-
-       ) /// End else
-       
-       }
-       {showModal && (
-         <Details job={selectedJob} handleCloseModal={handleCloseModal} />
-       )}
-     </div>
-
-    //////////////////////////////////////////////////////////////////////////////////////////
-
-    // <div className="mainDiv">
-
-
-    //   <div class="con">
-    //     {/* Custom */}
-    //         <div class="card" style={{ backgroundColor: "red" }}>
-    //               <h3 class="title">{obj.employer_name}</h3>
-    //               <h3 class="title" style={{ top: "270px" }}>{obj.job_city}</h3>
-    //               <h3 class="title" style={{ top: "310px" }}>{obj.job_country}</h3>
-    //               <h3 class="title" style={{ top: "350px" }}>{obj.obj.job_title}</h3>
-    //               <div class="bar">
-    //                     <div class="emptybar"></div>
-    //                     <div class="filledbar"></div>
-    //               </div>
-    //               <div class="circle">
-    //                     <circle class="stroke" cx="60" cy="60" r="50" >
-    //                       <img className="compImg" src={obj.employer_logo === "" ? Company : obj.employer_logo} alt="employer_logo" />
-    //                     </circle>
-    //               </div>
-    //               <button class="button"><span>Save</span></button>
-    //               <button class="button" style={{ left: "200px" }}><span>More</span></button>
-    //         </div>
-    //   </div>
-
-
-    // </div>
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    // <div className="mainDiv"> 
-    //   <div className="custmcontainer">
-    //     <div class="card">
-    //         <h3 class="title">Card 2</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    //     <div class="card">
-    //         <h3 class="title">Card 3</h3>
-    //         <div class="bar">
-    //             <div class="emptybar"></div>
-    //             <div class="filledbar"></div>
-    //         </div>
-    //         <div class="circle">
-    //             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-    //                 <circle class="stroke" cx="60" cy="60" r="50" />
-    //             </svg>
-    //         </div>
-    //     </div>
-    
-    
-    // </div>
-    
-    // </div>
-
+      )}
+      {showModal && (
+        <Details job={selectedJob} handleCloseModal={handleCloseModal} />
+      )}
+    </div>
   );
 };
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,80 +1,25 @@
-// import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-
 import './profile.css';
 
 const UserProfile = () => {
 
-  const { user, isAuthenticated } = useAuth0();
+  const { user } = useAuth0();
 
-  // const [userProfileData, setUserProfileData] = useState(null);
-  // const [showModal, setShowModal] = useState(true);
-  // const [formData, setFormData] = useState({
-  //   name: '',
-  //   email: '',
-  //   phone: '',
-  //   about: '',
-  //   imageOption: 'upload', // 'upload' or 'url'
-  //   imageFile: null, // Store the selected image file
-  //   imageUrl: '', // Store the image URL
-  //   imagePreview: null, // Store the image preview URL
-  // });
-
-  // const handleFormSubmit = (e) => {
-  //   e.preventDefault();
-  //   setUserProfileData(formData);
-  //   setShowModal(false);
-  // };
-
-  // const handleEditProfile = () => {
-  //   setShowModal(true);
-  // };
-
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setFormData((prevFormData) => ({
-  //     ...prevFormData,
-  //     [name]: value,
-  //   }));
-  // };
-
-  // const handleImageOptionChange = (e) => {
-  //   const { value } = e.target;
-  //   setFormData((prevFormData) => ({
-  //     ...prevFormData,
-  //     imageOption: value,
-  //     imageUrl: '',
-  //     imageFile: null,
-  //     imagePreview: null,
-  //   }));
-  // };
-
-  // const handleImageChange = (e) => {
-  //   const imageFile = e.target.files[0];
-  //   if (imageFile) {
-  //     setFormData((prevFormData) => ({
-  //       ...prevFormData,
-  //       imageFile: imageFile,
-  //       imagePreview: URL.createObjectURL(imageFile), // Generate a preview URL for the image
-  //     }));
-  //   }
-  // };
   return (
+    
     <div className="container mt-4">
-
-
       <div class="page-content page-container" id="page-content">
         <div class="padding">
           <div class="row container d-flex justify-content-center">
-            <div class="col-xl-6 col-md-12" style={{width :"80%" }}>
+            <div class="col-xl-6 col-md-12" style={{ width: "80%" }}>
               <div class="card user-card-full">
                 <div class="row m-l-0 m-r-0">
                   <div class="col-sm-4 bg-c-lite-green user-profile">
                     <div class="card-block text-center text-white">
                       <div class="m-b-25">
-                        <img  src={ user?.picture && user.picture}
+                        <img src={user?.picture && user.picture}
                           alt="User profile"
-                          class="img-radius"/>
+                          class="img-radius" />
                       </div>
                       <h6 class="f-w-600">{user?.name}</h6>
                       <p>Full Stack ...</p>
@@ -106,9 +51,9 @@ const UserProfile = () => {
                         </div>
                       </div>
                       <ul class="social-link list-unstyled m-t-40 m-b-10">
-                        <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
+                        <li><a href="www.facebook.com" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="www.twitter.com" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="www.instagram.com" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
                       </ul>
                     </div>
                   </div>
@@ -118,44 +63,6 @@ const UserProfile = () => {
           </div>
         </div>
       </div>
-
-
-      {/* <div className="profile">
-          <div className="left-side">
-            <div className="profile-image-container">
-              <img
-                src={ user?.picture && user.picture}
-                alt="User profile"
-                className="profile-image"
-              />
-            </div>
-          </div>
-          <div className="right-side">
-            <div className="profile-heading">
-              <h2>User Profile</h2>
-            </div>
-            <div className="profile-info">
-              <p>
-                <strong>Name:</strong> {user?.name}
-              </p>
-              <p>
-                <strong>Local:</strong> {user?.locale || `AR`}
-              </p>
-            </div>
-            <div className="profile-email">
-              <p>
-                <strong>Email:</strong> {user?.email || `${user?.nickname}@gmail.com`}
-              </p>
-            </div>
-            <div className="profile-phone">
-              <p>
-                <strong>Phone:</strong> {user?.phone_number || `07XXXXXXXX`}
-              </p>
-            </div>
-          </div>
-        </div> */}
-
-
     </div>
   );
 };
