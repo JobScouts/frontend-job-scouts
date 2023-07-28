0b17c6d4c08618105cbe5460aaffcab34a84cef4
MohamadSamara <mohamadsamara1211@gmail.com>
2023-07-28 21:39:27 +0000

    finishing .....

--- src/components/SavedCard/SavedCard.js
+++ src/components/SavedCard/SavedCard.js
@@ -8,18 +8,32 @@ import Oops from "../Assest/Oops.avif";
 import { Link } from 'react-router-dom';
 import Alert from 'react-bootstrap/Alert';
 
-
 const SavedCard = () => {
   const { user, isAuthenticated } = useAuth0();
+  const us = user?.sub || null; 
+  console.log(us);
+
   const [savedJob, setSavedJob] = useState([]);
   const [alertMessage, setAlertMessage] = useState(null);
   const [alertVariant, setAlertVariant] = useState('success');
 
+  useEffect(() => {
+    if (us) {
+      handleSavedJobs();
+    }
+  }, [us]);
+
   async function handleSavedJobs() {
-    const url = `${process.env.REACT_APP_SERVER_URL}/jobs`;
-    let response = await fetch(url);
-    let receivedData = await response.json();
-    setSavedJob(receivedData);
+    const url = `${process.env.REACT_APP_SERVER_URL}/jobs?sub=${us}`;
+    try {
+      let response = await fetch(url);
+      let receivedData = await response.json();
+      setSavedJob(receivedData);
+    } catch (error) {
+      console.log('Error:', error);
+      setAlertMessage('Error: Unable to fetch saved jobs.');
+      setAlertVariant('danger');
+    }
   }
 
   // To Open the apply link in a new tab
@@ -52,14 +66,17 @@ const SavedCard = () => {
     }
   }
 
-  useEffect(() => {
-    handleSavedJobs();
-  }, []);
+  // useEffect(() => {
+  //   handleSavedJobs();
+  // }, []);
 
-  const userSavedJobs = isAuthenticated && user && user.sub ? savedJob.filter((job) => job.sub === user.sub) : [];
+  const userSavedJobs = isAuthenticated && user && user.sub
+  ? savedJob.filter((job) => job.sub === user.sub)
+  : [];
+
 
   return (
-    <div className="center">
+    <div className="allJobSaved">
 
       {alertMessage && (
         <div className="fixed-alert">
@@ -71,25 +88,41 @@ const SavedCard = () => {
 
       {userSavedJobs.length > 0 ? (
         userSavedJobs.map((job) => (
-          <Card key={job.id} className="card">
-            <Card.Body>
-              <div className="card-content">
-                <img src={job.employer_logo === "" ? company : job.employer_logo} alt={job.employer_name} className="logo" />
-                <div className="card-text">
-                  <Card.Title>{job.employer_name}</Card.Title>
-                  <Card.Text>
-                    <p>{job.job_city}</p>
-                    <p>{job.job_title}</p>
-                  </Card.Text>
-                </div>
-              </div>
-              <div className="card-buttons">
-                <Button variant="primary" onClick={() => handleApplyToJob(job.job_apply_link)}>Apply To Job</Button>
-                <Button variant="primary" onClick={() => handleUnSaveJob(job.id)}>Un-Save</Button>
-              </div>
-            </Card.Body>
-          </Card>
-        ))
+
+/////// awe code
+<div className="samara">
+
+<section class="light">
+
+  {/* <div class="h1 text-center text-dark" id="pageHeaderTitle">My Cards Light</div> */}
+  <article class="postcard light blue">
+    {/* <a class="postcard__img_link" href="#"> */}
+      <img class="postcard__img" src={job.employer_logo} alt="Company_Image" />
+    {/* </a> */}
+    <div class="postcard__text t-dark">
+      <h1 class="postcard__title blue"><a href="#">{job.job_title}</a></h1>
+      <div class="postcard__subtitle small">
+        <time datetime="2020-05-25 12:00:00">
+          <i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+        </time>
+      </div>
+      <div class="postcard__bar"></div>
+      <div class="postcard__preview-txt">{job.job_highlights}</div>
+      <ul class="postcard__tagbox">
+        <li class="tag__item"><i class="fa-solid fa-trash-can"></i><button style={{background:"none" , border:"none"}}>Un-Save</button></li>
+        <li class="tag__item"><i class="fa-solid fa-arrow-right"></i><button style={{background:"none" , border:"none"}}>Apply</button></li>
+      </ul>
+    </div>
+  </article>
+
+</section>
+
+</div>
+))
+
+
+
+        ////// aws code
       ) : (
         <div className="no-jobs-container">
           <img
